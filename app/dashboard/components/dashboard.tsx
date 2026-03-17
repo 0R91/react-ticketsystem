09f0ccfc3916f5e42e1a7bf7803925874cd9724a
Oliver Racko <oliver-r@outlook.de>
2026-03-17 16:28:18 +0100

    Added TicketList module

--- app/dashboard/components/dashboard.tsx
+++ app/dashboard/components/dashboard.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import styles from './dashboard.module.css'
+import TicketList from './ticketList'
 import { useState } from 'react'
 
 type Props = {
@@ -14,7 +15,7 @@ export default function Dashboard({ tabs }: Props) {
 
   return (
     <div className={styles.dashboardContainer}>
-      <div className={styles.tabContainer}>
+      <div className={styles.tabHeader}>
         {tabsTopic.map((tabTopic) => (
           <button
             className={styles.button}
@@ -25,20 +26,25 @@ export default function Dashboard({ tabs }: Props) {
           </button>
         ))}
       </div>
-      <div
-        className={`${styles.tabContent} ${tab === 'Ticket List' ? styles.active : styles.hidden}`}
-      >
-        Ticket List
-      </div>
-      <div
-        className={`${styles.tabContent} ${tab === 'Create new Ticket' ? styles.active : styles.hidden}`}
-      >
-        Create new Ticket
-      </div>
-      <div
-        className={`${styles.tabContent} ${tab === 'Tickets Done' ? styles.active : styles.hidden}`}
-      >
-        Tickets Done
+
+      <div className={styles.tabContentContainer}>
+        <div
+          className={`${styles.tabContent} ${tab === 'Ticket List' ? styles.active : styles.hidden}`}
+        >
+          Ticket List
+        </div>
+
+        <div
+          className={`${styles.tabContent} ${tab === 'Create new Ticket' ? styles.active : styles.hidden}`}
+        >
+          Create new Ticket
+        </div>
+
+        <div
+          className={`${styles.tabContent} ${tab === 'Tickets Done' ? styles.active : styles.hidden}`}
+        >
+          Tickets Done
+        </div>
       </div>
     </div>
   )
