0f49ed4fc22c81fa524d108e1a51219f4fa07b44
Oliver Racko <oliver-r@outlook.de>
2026-03-28 18:12:44 +0100

    Implemented Supabase-backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
-  return <></>
+import { useEffect } from 'react'
+import { supabase } from '@/lib/supabase'
+
+export default function Page() {
+  useEffect(() => {
+    const run = async () => {
+      const { data, error } = await supabase
+        .from('ticket')
+        .insert({
+          title: 'Test Ticket',
+          description: 'Mein erstes Ticket',
+          status: 'open',
+        })
+        .select()
+
+      console.log('inserted row:', data)
+      console.log('error:', error)
+    }
+
+    run()
+  }, [])
+
+  return <div>Check console</div>
 }
